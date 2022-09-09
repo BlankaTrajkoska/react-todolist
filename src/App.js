470efe8f709b696291a2d18ba8eb93c15cdf1b66
BlankaTrajkoska <blanka.ricko@gmail.com>
2022-09-09 17:29:37 +0200

    added all functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./dist/css/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { useState } from "react";
 import ToDoList from "./components/ToDoList";
 import ToDoForm from "./components/ToDoForm";
-//import { useEffect } from "react";
+
 function App() {
-  // const state = {
   const todoItems = [
     { id: 1000, task: "Walk the dog", done: false },
     { id: 2000, task: "Water the flower", done: true },
   ];
-  // };
 
-  const [todoList, setToDoList] = useState(todoItems);
+  const [todoList, setTodoList] = useState(todoItems);
   const [text, setText] = useState("");
 
   const handleChange = (e) => {
@@ -22,47 +21,55 @@ function App() {
   };
 
   const handleAdd = () => {
-    const newToDo = {
-      id: Math.floor(Math.random() * 1000),
-      task: text,
-      done: false,
-    };
+    if (text) {
+      const newToDo = {
+        id: Math.floor(Math.random() * 1000),
+        task: text,
+        done: false,
+      };
 
-    const newToDoList = [...todoList, newToDo];
-    setToDoList(newToDoList);
-    console.log(todoList);
-    setText("");
+      const newToDoList = [...todoList, newToDo];
+      setTodoList(newToDoList);
+
+      setText("");
+    }
   };
 
   const handleDelete = (id) => {
-    const newList = todoList.filter((i) => i.id !== id);
-    setToDoList(newList);
+    const newList = todoList.filter((each) => each.id !== id);
+    setTodoList(newList);
   };
 
+  const handleClick = (index) => {
+    const newList = [...todoList];
+    newList[index].done = !newList[index].done;
+    setTodoList(newList);
+  };
+  console.log(todoList);
   return (
-    <Router>
-      <div className="App">
-        <div>
-          <h1>Todo List</h1>
-          <div>
-            <ul>
-              {todoList.map((todoItem) => (
-                <ToDoList
-                  key={todoItem.id}
-                  todoItem={todoItem}
-                  handleDelete={handleDelete}
-                />
-              ))}
-            </ul>
-          </div>
+    <div className="App">
+      <div className="headlist">
+        <h1 className="title">Todo List</h1>
+        <div className="list-container">
+          {todoList.map((todoItem, index) => (
+            <ToDoList
+              key={todoItem.id}
+              todoItem={todoItem}
+              handleDelete={handleDelete}
+              handleClick={handleClick}
+              index={index}
+            />
+          ))}
         </div>
+      </div>
+      <div className="form">
         <ToDoForm
           handleChange={handleChange}
           handleAdd={handleAdd}
           text={text}
         />
       </div>
-    </Router>
+    </div>
   );
 }
 
--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-//import { useState } from "react";
+
 const ToDoForm = ({ handleAdd, text, handleChange }) => {
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          value={text}
-          onChange={handleChange}
-          placeholder="add a new todo..."
-        />
-        <button onClick={() => handleAdd()}>Add</button>
-      </form>
+    <div className="form-container">
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="add a new todo..."
+      />
+      <button onClick={() => handleAdd()}>Add</button>
     </div>
   );
 };
